3cae949731f16e9b894ff96cbc4ea98fdabc84e1
B0rjomi <valerkalifanov@gmail.com>
2022-11-15 13:08:45 +0300

    incompletely solved events task

--- src/Events.js
+++ src/Events.js
@@ -2,14 +2,30 @@
    1. Создайте функцию createButton(). Необходимо, чтобы эта функция осуществила вставку в body тег button с текстом: "Удали меня".
       При клике по button удалить этот button.
 */
-export function createButton() {}
+export function createButton() {
+    let butt = document.createElement('button');
+    butt.innerHTML = 'Удали меня';
+    document.body.appendChild(butt);
+    butt.addEventListener('click', () => butt.remove());
+}
 
 /*
    2. Создайте функцию createArrList(arr), в которую передается 1 параметр: arr - массив строк.
       Функция выводит этот массив в виде маркированного списка внутри тега body.
       При наведении курсора мыши на элемент списка у этого элемента создается атрибут title, в котором записан его текст.
 */
-export function createArrList(arr) {}
+export function createArrList(arr) {
+    let ul_list = document.createElement('ul');
+    document.body.append(ul_list);
+    for (let i = 0; i < arr.length; i++) {
+        let li_list = document.createElement('li');
+        li_list.innerHTML = arr[i];
+        li_list.addEventListener('mouseover', () => {
+            li_list.setAttribute('title', arr[i]);
+        });
+        ul_list.append(li_list);
+    }
+}
 
 /*
    3. Создайте функцию createLink(), которая сгенерирует следующую разметку и вставит ее в body:
